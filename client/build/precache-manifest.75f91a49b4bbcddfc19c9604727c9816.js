f4fdac94988dd4058cc4cc749c027647c389204d
Todd Leslie <spotontechnology@gmail.com>
2020-05-28 20:26:24 -0600

    Boiler plate

---
+++ client/build/precache-manifest.75f91a49b4bbcddfc19c9604727c9816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbe0011520f32037ea20",
+    "url": "/static/css/main.8bb2d1de.chunk.css"
+  },
+  {
+    "revision": "dbe0011520f32037ea20",
+    "url": "/static/js/main.8e40f6fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "f9d9a0dfe6a0c26d1b26e3dd57cf4401",
+    "url": "/index.html"
+  }
+];
