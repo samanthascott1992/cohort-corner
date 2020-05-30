24f48f24873ff72f2d1092d378d64884867418d6
Todd Leslie <spotontechnology@gmail.com>
2020-05-30 14:35:56 -0600

    auth0 add

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,68 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+//import library from "./client/public/images/library.png"
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+
+
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div> loading...</div>;
+  }
+
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header>
+        <NavBar />
+        <Router>
+       <div>
+         <Nav />
+         <Switch>
+           <Route exact path={["/", "/books"]}>
+             <Books />
+           </Route>
+           <Route exact path="/books/:id">
+             <Detail />
+           </Route>
+           <Route>
+             <NoMatch />
+           </Route>
+         </Switch>
+       </div>
+     </Router>
+      </header>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path={["/", "/books"]}>
+//             <Books />
+//           </Route>
+//           <Route exact path="/books/:id">
+//             <Detail />
+//           </Route>
+//           <Route>
+//             <NoMatch />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
