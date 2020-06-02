6d04bea4fc2eec41e1985e14ce01bc2c55879c9b
Sonia Trimble <smtmarie@gmail.com>
2020-06-02 12:01:15 -0600

    database url work

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -25,98 +25,98 @@ const linkSeed = [{
     {
         category: "MongoDB",
         subject: "MongoDB Query Operators",
-        url: " ",
+        url: "https://docs.mongodb.com/manual/reference/operator/query/",
         synopsis: "Descriptive listing of Query and Projection operators.",
         date: new Date(Date.now())
     },
     {
         category: "MongoDB",
         subject: "Mongojs Collection Methods",
-        url: " ",
+        url: "https://github.com/mongo-js/mongojs#api",
         synopsis: "A tutorial of Node.js module for MongoDB, that emulates the official MongoDB API as much as possible. It wraps MongoDB-native and is available through npm",
         date: new Date(Date.now())
     },
     {
         category: "Express",
         subject: "Express with API Reference",
-        url: " ",
+        url: "http://expressjs.com/en/4x/api.html#res.end",
         synopsis: "Extensive listing of syntax resources for middleware function in Express.",
         date: new Date(Date.now())
     },
     {
         category: "Express",
         subject: "Express Tutorial for Routes and Controllers",
-        url:  " ",
+        url: "https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes",
         synopsis: "This tutorial provides support in setting up routes (URL handling code) with handler functions for resources needed concerning endpoints.",
         date: new Date(Date.now())
     },
     {
         category: "Express",
-        subject: "Express Response Methods", 
-        url:  "",      
+        subject: "Express Response Methods",
+        url: "https://expressjs.com/en/guide/routing.html#response-methods",
         synopsis: "Tutorial on the various methods of routing, and how an application's endpoints (URIs) respond to client requests.",
         date: new Date(Date.now())
     },
     {
         category: "Express",
-        subject: "Using Express Middleware", 
-        url: " ",
+        subject: "Using Express Middleware",
+        url: "https://expressjs.com/en/guide/using-middleware.html",
         synopsis: "A tutorial in helping to convey the logic of how Express applications are essentially a series of middleware function calls.",
         date: new Date(Date.now())
     },
     {
         category: "ReactJS",
-        subject:  "Latest ReactJS Examples", 
-        url: " ",
+        subject: "Latest ReactJS Examples",
+        url: "https://react.rocks/",
         synopsis: "Great examples and inspiration in building ReactJS applications.",
         date: new Date(Date.now())
     },
     {
         category: "ReactJS",
-        subject:  "React State vs Props Explained",
-        url: " ",
+        subject: "React State vs Props Explained",
+        url: "https://codeburst.io/react-state-vs-props-explained-51beebd73b21",
         synopsis: "Basic tutorial explaining and comparing React components to plain functions.",
         date: new Date(Date.now())
     },
     {
         category: "ReactJS",
         subject: "Rules of Hooks in React",
-        url: " ",
+        url: "https://reactjs.org/docs/hooks-rules.html",
         synopsis: "Tutorial on how to follow the rules in utilizing the JavaScript function of hooks in React.",
         date: new Date(Date.now())
     },
-    { 
+    {
         category: "ReactJS",
         subject: "Fragments-React",
-        url: " ",
+        url: "https://reactjs.org/docs/fragments.html",
         synopsis: "Tutorial on the motivation, usage and syntax of fragments in ReactJS.",
         date: new Date(Date.now())
     },
     {
         category: "Node.js",
         subject: "What exactly is Node.js?",
-        url: " ",        
+        url: "https://www.freecodecamp.org/news/what-exactly-is-node-js-ae36e97449f5/",
         synopsis: "Informative tutorial explaining how Node.js' run time environment includes everything one would need to execute a program written in JavaScript.",
         date: new Date(Date.now())
     },
     {
         category: "Node.js",
         subject: "Read Files with Node.js",
-        url: " ",
+        url: "https://stackabuse.com/read-files-with-node-js/",
         synopsis: "An informative tutorial on how to open and read files utilizing the fs method in Node.js.",
         date: new Date(Date.now())
     },
     {
         category: "Node.js",
         subject: "Understanding module.exports and exports in Node.js",
-        url: " ",
+        url: "https://www.sitepoint.com/understanding-module-exports-exports-node-js/",
         synopsis: "Informing tutorial on how modules essentially make our lives as developers easier, as we can utilize them them to augment our applications with functionality that we haven’t had to write ourselves.",
         date: new Date(Date.now())
     },
     {
         category: "Node.js",
-        subject:  "How to Deploy a Node.js App to Heroku",
-        url: " ",
+        subject: "How to Deploy a Node.js App to Heroku",
+        url: "https://scotch.io/tutorials/how-to-deploy-a-node-js-app-to-heroku",
         synopsis: "Tutorials on implementation and tools needed to deploy a Node.js application to Heroku.",
         date: new Date(Date.now())
     },
