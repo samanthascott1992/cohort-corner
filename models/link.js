3053b1b7e52489f1a65e44afab48079d1e9e5a00
Sonia Trimble <smtmarie@gmail.com>
2020-06-01 18:20:15 -0600

    seeds work

--- models/link.js
+++ models/link.js
@@ -2,13 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const linkSchema = new Schema({
-  category: { type: String, required: true },
-  subject: { type: String, required: true },
-  url: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+    category: { type: String, required: true },
+    subject: { type: String, required: true },
+    url: { type: String, required: true },
+    synopsis: String,
+    date: { type: Date, default: Date.now }
 });
 
 const Link = mongoose.model("Link", linkSchema);
 
 module.exports = Link;
+
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// let Todo = new Schema({
+//     todo_description: {
+//         type: String
+//     },
+//     todo_responsible: {
+//         type: String
+//     },
+//     todo_priority: {
+//         type: String
+//     },
+//     todo_completed: {
+//         type: Boolean
+//     }
+// });
+// module.exports = mongoose.model('Cohort', Cohort);
