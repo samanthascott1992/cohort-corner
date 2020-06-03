e88c74dbc255c9d43d5169bbb2b4c1c399817825
Sonia Trimble <smtmarie@gmail.com>
2020-06-03 16:24:18 -0600

    data funnnnnnn

--- client/src/pages/Links.js
+++ client/src/pages/Links.js
@@ -11,153 +11,299 @@ import ract from "../Images/react.png";
 import node from "../Images/node.png";
 import Nav from "../components/Nav";
 import axios from "axios";
-import { useAuth0 } from "../react-auth0-spa";
+import { useAuth0 } from "../react-auth0-spa"
+
 
 function Links() {
-  // Setting our component's initial state
-  const [links, setLinks] = useState([]);
-  const [formObject, setFormObject] = useState({});
+    // Setting our component's initial state
+    const [links, setLinks] = useState([])
+    const [formObject, setFormObject] = useState({})
 
-  // Load all links and store them with setLinks
-  useEffect(() => {
-    loadLinks();
-  }, []);
+    // Load all links and store them with setLinks
+    useEffect(() => {
+        loadLinks()
+    }, [])
 
-  // Loads all links and sets them to links
-  function loadLinks() {
-    API.getLinks()
-      .then((res) => setLinks(res.data))
-      .catch((err) => console.log(err));
-  }
+    // Loads all links and sets them to links
+    function loadLinks() {
+        API.getLinks()
+            .then(res =>
+                setLinks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
 
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value });
-  }
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({...formObject, [name]: value })
+    };
 
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.url && formObject.subject) {
-      API.saveLink({
-        category: formObject.category,
-        subject: formObject.subject,
-        url: formObject.url,
-        synopsis: formObject.synopsis,
-      })
-        .then((res) => loadLinks())
-        .catch((err) => console.log(err));
+    // When the form is submitted, use the API.saveBook method to save the book data
+    // Then reload books from the database
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.url && formObject.subject) {
+            API.saveLink({
+                    category: formObject.category,
+                    subject: formObject.subject,
+                    url: formObject.url,
+                    synopsis: formObject.synopsis
+                })
+                .then(res => loadLinks())
+                .catch(err => console.log(err));
+        }
+        loadLinks();
+    };
+
+    const viewLinks = (data) => {
+
+        console.log(data, "Enter Here")
+        const { user } = useAuth0()
+
+        const [setArray] = useState([])
+
+        const getLinks = () => {
+
+            if (user) {
+
+                axios.get("api/links/category/mongodb")
+                    .then(res => {
+
+                        setArray(res.data)
+                    })
+
+            }
+        }
+        getLinks()
     }
     loadLinks();
-  }
+}
 
-  const viewLinks = ({ id }) => {
+const viewLinks = ({ id }) => {
     const { user } = useAuth0();
 
+    <<
+    << << < Updated upstream
     const [array, setArray] = useState([]);
 
     const getLinks = () => {
-      if (user) {
-        axios.get("api/links/category/mongodb").then((res) => {
-          setArray(res.data);
-        });
-      }
+        if (user) {
+            axios.get("api/links/category/mongodb").then((res) => {
+                setArray(res.data);
+            });
+        }
     };
-  };
+};
 
-  return (
-    <Container fluid>
-      <Nav />
-      <Row>
-        <Col size="md-6">
-          <Jumbotron>
-            <h2> Subjects to Choose From </h2>
-          </Jumbotron>
-          <button onClick={viewLinks}>
-            {" "}
-            MongoDB <img
-              src={mongo}
-              width="50"
-              height="50"
-              alt="mongodb"
-            />{" "}
-          </button>{" "}
-          <button onClick={viewLinks}>
-            {" "}
-            Express <img
-              src={nExpress}
-              width="50"
-              height="50"
-              alt="express"
-            />{" "}
-          </button>{" "}
-          <button onClick={viewLinks}>
-            {" "}
-            React <img src={ract} width="50" height="50" alt="react" />{" "}
-          </button>{" "}
-          <button onClick={viewLinks}>
-            {" "}
-            Node <img src={node} width="50" height="50" alt="node" />{" "}
-          </button>
-          <Jumbotron>
-            <h1> Submit a New Link for Reference </h1>{" "}
-          </Jumbotron>{" "}
-          <form>
-            <Input
-              onChange={handleInputChange}
-              name="category"
-              placeholder="Please Pick a Category to Submit to!"
-            />
-            <Input
-              onChange={handleInputChange}
-              name="subject"
-              placeholder="Subject (required)"
-            />
-            <Input
-              onChange={handleInputChange}
-              name="url"
-              placeholder="URL (required)"
-            />
-            <TextArea
-              onChange={handleInputChange}
-              name="synopsis"
-              placeholder="Synopsis (Optional)"
-            />
-            <FormBtn
-              disabled={
-                !(formObject.category && formObject.subject && formObject.url)
-              }
-              onClick={handleFormSubmit}
-            >
-              Submit New Reference Link{" "}
-            </FormBtn>{" "}
-          </form>
-        </Col>{" "}
-        <Col size="md-6 sm-12">
-          <Jumbotron>
-            <h1> Reference Links </h1>{" "}
-          </Jumbotron>{" "}
-          {links.length ? (
-            <List>
-              {" "}
-              {links.map((link) => (
-                <ListItem key={link._id}>
-                  <Link to={"/links/" + link._id}>
-                    <strong> {link.subject} </strong>{" "}
-                  </Link>{" "}
-                </ListItem>
-              ))}{" "}
-            </List>
-          ) : (
-            <h3> No Results to Display </h3>
-          )}{" "}
-        </Col>{" "}
-      </Row>{" "}
-    </Container>
-  );
+return ( <
+    Container fluid >
+    <
+    Nav / >
+    <
+    Row >
+    <
+    Col size = "md-6" >
+    <
+    Jumbotron >
+    <
+    h2 > Subjects to Choose From < /h2> <
+    /Jumbotron> <
+    button onClick = { viewLinks } > { " " }
+    MongoDB < img src = { mongo }
+    width = "50"
+    height = "50"
+    alt = "mongodb" /
+    > { " " } <
+    /button>{" "} <
+    button onClick = { viewLinks } > { " " }
+    Express < img src = { nExpress }
+    width = "50"
+    height = "50"
+    alt = "express" /
+    > { " " } <
+    /button>{" "} <
+    button onClick = { viewLinks } > { " " }
+    React < img src = { ract }
+    width = "50"
+    height = "50"
+    alt = "react" / > { " " } <
+    /button>{" "} <
+    button onClick = { viewLinks } > { " " }
+    Node < img src = { node }
+    width = "50"
+    height = "50"
+    alt = "node" / > { " " } <
+    /button> <
+    Jumbotron >
+    <
+    h1 > Submit a New Link
+    for Reference < /h1>{" "} <
+    /Jumbotron>{" "} <
+    form >
+    <
+    Input onChange = { handleInputChange }
+    name = "category"
+    placeholder = "Please Pick a Category to Submit to!" /
+    >
+    <
+    Input onChange = { handleInputChange }
+    name = "subject"
+    placeholder = "Subject (required)" /
+    >
+    <
+    Input onChange = { handleInputChange }
+    name = "url"
+    placeholder = "URL (required)" /
+    >
+    <
+    TextArea onChange = { handleInputChange }
+    name = "synopsis"
+    placeholder = "Synopsis (Optional)" /
+    >
+    <
+    FormBtn disabled = {!(formObject.category && formObject.subject && formObject.url)
+    }
+    onClick = { handleFormSubmit } >
+    Submit New Reference Link { " " } <
+    /FormBtn>{" "} <
+    /form> <
+    /Col>{" "} <
+    Col size = "md-6 sm-12" >
+    <
+    Jumbotron >
+    <
+    h1 > Reference Links < /h1>{" "} <
+    /Jumbotron>{" "} {
+        links.length ? ( <
+            List > { " " } {
+                links.map((link) => ( <
+                    ListItem key = { link._id } >
+                    <
+                    Link to = { "/links/" + link._id } >
+                    <
+                    strong > { link.subject } < /strong>{" "} <
+                    /Link>{" "} <
+                    /ListItem>
+                ))
+            } { " " } <
+            /List>
+        ) : ( <
+            h3 > No Results to Display < /h3>
+        )
+    } { " " } <
+    /Col>{" "} <
+    /Row>{" "} <
+    /Container>
+);
 }
 
 export default Links;
 
+===
+=== =
+return (
+
+    <
+    Container fluid >
+    <
+    Nav / >
+    <
+    Row >
+    <
+    Col size = "md-6" >
+    <
+    Jumbotron >
+    <
+    h2 > Subjects to Choose From < /h2>
+
+    <
+    /Jumbotron>
+
+    <
+    button onClick = { viewLinks } > MongoDB < img src = { mongo }
+    width = "50"
+    height = "50"
+    alt = "mongodb" / > < /button>  <
+    button onClick = { viewLinks } > Express < img src = { nExpress }
+    width = "50"
+    height = "50"
+    alt = "express" / > < /button>  <
+    button onClick = { viewLinks } > React < img src = { ract }
+    width = "50"
+    height = "50"
+    alt = "react" / > < /button>  <
+    button onClick = { viewLinks } > Node < img src = { node }
+    width = "50"
+    height = "50"
+    alt = "node" / > < /button> 
+
+    <
+    Jumbotron >
+    <
+    h1 > Submit a New Link
+    for Reference < /h1> < /
+    Jumbotron > <
+    form >
+    <
+    Input onChange = { handleInputChange }
+    name = "category"
+    placeholder = "Please Pick a Category to Submit to!" /
+    >
+    <
+    Input onChange = { handleInputChange }
+    name = "subject"
+    placeholder = "Subject (required)" /
+    >
+    <
+    Input onChange = { handleInputChange }
+    name = "url"
+    placeholder = "URL (required)" /
+    >
+    <
+    TextArea onChange = { handleInputChange }
+    name = "synopsis"
+    placeholder = "Synopsis (Optional)" /
+    >
+    <
+    FormBtn disabled = {!(formObject.category && formObject.subject && formObject.url) }
+    onClick = { handleFormSubmit } >
+    Submit New Reference Link <
+    /FormBtn> < /
+    form >
+
+    <
+    /Col> <
+    Col size = "md-6 sm-12" >
+    <
+    Jumbotron >
+    <
+    h1 > Reference Links < /h1> < /
+    Jumbotron > {
+        links.length ? ( <
+            List > {
+                links.map(link => ( <
+                    ListItem key = { link._id } >
+                    <
+                    Link to = { "/links/" + link._id } >
+                    <
+                    strong > { link.subject } <
+                    /strong> < /
+                    Link > <
+                    /ListItem>
+                ))
+            } <
+            /List>
+        ) : ( <
+            h3 > No Results to Display < /h3>
+        )
+    } <
+    /Col> < /
+    Row > <
+    /Container>
+);
+
+}
+
+export default Links; >>>
+>>> > Stashed changes
