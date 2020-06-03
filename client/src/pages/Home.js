366299dd5dbbdcbfc730f5d4e221bb43aafd7c00
Todd Leslie <spotontechnology@gmail.com>
2020-06-03 14:34:25 -0600

    updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from "react";
-import books from "../Images/library.png";
+import vbooks from "../Images/library.mp4";
 import { useAuth0 } from "../react-auth0-spa";
-// import Toast from "react-bootstrap/Toast;"
+import { Player } from "video-react"
 
 
 const styles = {
@@ -22,18 +22,19 @@ function Home() {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
     return (
 
-        <img src={books} alt="books" className="booksImg" style= {styles.img} />
+      <Player
+      playsInline
+      src={vbooks}
+      autoplay
+      fluid={false}
+      width={100}
+      height={50}
+    />
+
 
     );
 };
 
-// <Toast>
-//   <Toast.Header>
-//     <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-//     <strong className="mr-auto">Bootstrap</strong>
-//     <small>11 mins ago</small>
-//   </Toast.Header>
-//   <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-// </Toast>
+
 
    export default Home;
