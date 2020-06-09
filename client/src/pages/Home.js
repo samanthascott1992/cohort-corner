da1bb91b92c6d3f372357606cd37f7bcc6a063c8
Samantha Scott <Samanthascott1992@gmail.com>
2020-06-09 10:05:38 -0600

    working on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,12 @@
 import React, {useState, useEffect } from "react";
 // import vbooks from "../Images/library.mp4";
 // import { Player } from "video-react"
-import Library from "../Images/library.png";
+import cohortCorner from "../Images/cohortcorner.png";
 import { useAuth0 } from "../react-auth0-spa";
 
 
 const styles = {
-  img: {
-    width: "100%",
-    height: "80%",
-    zIndex: 1,
-  },
+
   button: {
     zIndex: -1,
     float: "center",
@@ -22,7 +18,7 @@ const styles = {
 function Home() {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
     return (
-      <img src={Library} alt="library" />
+      <img src={cohortCorner} alt="library" />
 
 
 
